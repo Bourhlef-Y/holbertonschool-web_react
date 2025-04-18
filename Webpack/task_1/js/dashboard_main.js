0a591b2358f6509923839fdc5b6be67c388cfea2
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-04-18 10:51:36 +0200

    Refactor build script and improve click counter functionality in dashboard_main.js

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -8,17 +8,17 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
+// Variable pour suivre le nombre de clics
+let count = 0;
+
 // Fonction pour mettre à jour le compteur
 function updateCounter() {
-  let count = 0;
-  return function() {
-    count += 1;
-    $('#count').text(`${count} clicks on the button`);
-  };
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
 }
 
 // Utilisation de la fonction debounce de Lodash pour éviter le spam
-const debouncedUpdateCounter = _.debounce(updateCounter(), 500);
+const debouncedUpdateCounter = _.debounce(updateCounter, 500);
 
 // Ajout de l'événement de clic sur le bouton
 $('button').on('click', debouncedUpdateCounter); 
